fc16ffd2374b5ad74fa0d3528f44056642548a88
Pratham <prathamsinghal2003@gmail.com>
2023-07-28 22:23:47 +0530

    feat: add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
 async function main() {
-  await lock.waitForDeployment();
-}
+  const [deployer] = await ethers.getSigners();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const token = await ethers.deployContract("PFP3NFT");
+
+  await token.waitForDeployment();
+
+  console.log("Token address:", await token.getAddress());
+}
+// 0xD947A6b8DA2F0e5BCDEDca7746dA4Ea562A26240
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
